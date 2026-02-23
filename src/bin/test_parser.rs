46ff8d49c0cb70f8d97e13b321e61f54a0b4c29f
Shohei Fujii <fujii.shohei@gmail.com>
2026-02-23 20:16:06 +0900

    remove unnecessary file

--- src/bin/test_parser.rs
+++
@@ -1,6 +0,0 @@
-use patto::parser::parse_text;
-
-fn main() {
-    let text1 = "相手のやりたいことを知り、働き方を改善し、[* 生産性を高められる]ようにする";
-    println!("Text 1:\n{}", serde_json::to_string_pretty(&parse_text(text1).ast).unwrap());
-}
